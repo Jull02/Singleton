4e24eeddb65ce2b02554af148b23ed5b1c3f3163
Julissa Ruales <julissa97_ruales@hotmail.com>
2023-06-08 10:02:31 -0500

    Trabajo en clase

---
+++ Pages/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace CRUD.Pages
+{
+	public class Singleton
+	{
+		private readonly static SqlConnection _instance = new SqlConnection(ConfigurationManager.ConnectionStrings["conexion"].ConnectionString);
+
+		public static SqlConnection Instance
+		{
+			get
+			{
+				return _instance;
+			}
+		}
+
+		private Singleton()
+		{
+		}
+	}
+}
